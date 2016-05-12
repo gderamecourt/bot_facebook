d6081db52a07277ed4f8007a39ab993ffdcdaad2
gabouch <gabriel.deramecourt@gmail.com>
2016-05-12 22:28:36 +0200

    Fix

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 // "use strict";
 
-const http = require('http');
-const Bot = require('messenger-bot');
+var http = require('http');
+var Bot = require('messenger-bot');
 var response = require('./response');
+
 const FB_TOKEN = process.env.FB_TOKEN;
 const FB_VERIFY = process.env.FB_VERIFY;
 
-let bot = new Bot({
+var bot = new Bot({
     token: FB_TOKEN,
     verify: FB_VERIFY
 });
 
 bot.on('error', (err) => {
-    console.log(err.message)
+    console.log(err.message);
 });
 
 bot.on('message', (payload, reply) => {
     let text = payload.message.text
     reply({
-        text
-
-
+        text;
     }, (err) => {
         if (err) throw err;
 
         console.log(`Echoed back : ${text}`);
-    })
-})
+    });
+});
 http.createServer(bot.middleware()).listen(process.env.PORT);
 console.log('Server is running.');
