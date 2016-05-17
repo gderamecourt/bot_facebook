924e453ba789975fb6dbbfac6483283ba083077b
gabouch <gabriel.deramecourt@gmail.com>
2016-05-17 21:56:24 +0200

    base propre

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 "use strict";
 
-const http = require('http');
-const Bot = require('messenger-bot');
+const http = require('http')
+const Bot = require('messenger-bot')
 
-const FB_TOKEN = process.env.FB_TOKEN;
-const FB_VERIFY = process.env.FB_VERIFY;
+const FB_TOKEN = process.env.FB_TOKEN
+const FB_VERIFY = process.env.FB_VERIFY
 
 let bot = new Bot({
     token: FB_TOKEN,
     verify: FB_VERIFY
-});
+})
 
 bot.on('error', (err) => {
     console.log(err.message)
-});
+})
 
 bot.on('message', (payload, reply) => {
-    let text = payload.message.text;
+    let text = payload.message.text
     reply({
-        text;
+        text
     }, (err) => {
-        if (err) throw err;
+        if (err) throw err
 
-        console.log(`Echoed back : ${text}`);
-    });
-});
-http.createServer(bot.middleware()).listen(process.env.PORT);
-console.log('Server is running.');
+        console.log(`Echoed back : ${text}`)
+    })
+})
+http.createServer(bot.middleware()).listen(process.env.PORT)
+console.log('Server is running.')
