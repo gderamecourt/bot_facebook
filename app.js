5e5e12a7b26e133df4310850a7fb6cd347c49d10
gabouch <gabriel.deramecourt@gmail.com>
2016-05-18 21:50:45 +0200

    test message dans wehook

--- app.js
+++ app.js
@@ -37,6 +37,20 @@ app.get('/webhook', function (req, res) {
   }
 });
 
+// Action on posting a message 
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
