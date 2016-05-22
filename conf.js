5338745ce1150b5a13601920dd01b0a0601376c9
gabouch <gabriel.deramecourt@gmail.com>
2016-05-22 14:40:10 +0200

    Addition of meteo callback

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 var conf = module.exports = {
 	FB_MESSAGE_URL: 'https://graph.facebook.com/v2.6/me/messages',
-	VERIFY_TOKEN: process.env.FB_TOKEN,
-	PROFILE_TOKEN: process.env.FB_VERIFY,
+	VERIFY_TOKEN: process.env.FB_VERIFY,
+	PROFILE_TOKEN: process.env.FB_TOKEN,
 	PORT: '3000'
 };
