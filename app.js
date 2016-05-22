458b489265bace050240a056a9204dd33eeda76a
gabouch <gabriel.deramecourt@gmail.com>
2016-05-22 13:10:18 +0200

    retest

--- app.js
+++ app.js
@@ -1,107 +1,71 @@
+var request = require('request');
+var http = require('http');
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compression = require('compression');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var response = require('./routes/response.js')
+var conf = require('./conf');
 
 var app = express();
-
-const FB_TOKEN = process.env.FB_TOKEN;
-const FB_VERIFY = process.env.FB_VERIFY;
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(compression());
+app.set('case sensitive routing', true);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+var httpServer = http.createServer(app);
 
-// verification of the token
-app.get('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === FB_VERIFY) {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.send('Error, wrong validation token');    
-  }
+app.get('/', function (req, res, next) {
+  res.send('Welcome to Facebook Messenger Bot. This is root endpoint');
 });
 
-// Action on posting a message 
-app.post('/webhook/', function (req, res) {
-  messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
-    if (event.message && event.message.text) {
-      text = event.message.text;
-      console.log('TEXT : ' + text);
-      //function 
-      messageData = {
-        text:text
-      }
-      request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:FB_TOKEN},
-        method: 'POST',
-        json: {
-          recipient: {id:sender},
-          message: messageData,
-        }
-      }, function(error, response, body) {
-        if (error) {
-          console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-          console.log('Error: ', response.body.error);
-        }
-      });
-      console.log('FIN DE LA REQUEST');
+app.get('/webhook/', handleVerify);
+app.post('/webhook/', receiveMessage);
 
+function handleVerify(req, res, next){
+  if (req.query['hub.verify_token'] === conf.VERIFY_TOKEN) {
+    return res.send(req.query['hub.challenge']);
+  }
+  res.send('Validation failed, Verify token mismatch');
+}
+
+function receiveMessage(req, res, next){
+  var message_instances = req.body.entry[0].messaging;
+  message_instances.forEach(function(instance){
+    var sender = instance.sender.id;
+    if(instance.message && instance.message.text) {
+      var msg_text = instance.message.text;
+      sendMessage(sender, msg_text, true);
+    }
+  });
+  res.sendStatus(200);
+}
+
+function sendMessage(receiver, data, isText){
+  var payload = {};
+  payload = data;
+  
+  if(isText) {
+    payload = {
+      text: data
     }
   }
-  res.sendStatus(200);
-});
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+  request({
+    url: conf.FB_MESSAGE_URL,
+    method: 'POST',
+    qs: {
+      access_token: conf.PROFILE_TOKEN
+    },
+    json: {
+      recipient: {id: receiver},
+      message: payload
+    }
+  }, function (error, response) {
+    if(error) console.log('Error sending message: ', error);
+    if(response.body.error) console.log('Error: ', response.body.error);
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+var port = conf.PORT;
+httpServer.listen(port, function () {
+  console.log("Express http server listening on port " + port);
 });
-
-module.exports = app;
---
+++ conf.js
@@ -0,0 +1,6 @@
+var conf = module.exports = {
+	FB_MESSAGE_URL: 'https://graph.facebook.com/v2.6/me/messages',
+	VERIFY_TOKEN: 'Daneel_Olivaw',
+	PROFILE_TOKEN: 'EAAZAZAZClJUny8BAK0XZCdnizmebomF3FtM3WSwZBzxX8sKQfVrSUjpp9sATy7hvkj6J7HjBhXgnxhB2OgBjrtTCzYu1IxAS8o5ZAoqby4b1ZAXVpZAkLicF2uRZA8D6LT2kC8unKHYEcQvNd45oICfyfencLZBcxW9NHmIoLGhzsrwgZDZD',
+	PORT: '3000'
+};
