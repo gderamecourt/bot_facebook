27718287b80a224601c2f161eaa31ea501bcc8e1
gabouch <gabriel.deramecourt@gmail.com>
2016-05-30 22:20:12 +0200

    got the city from the internet

--- routes/processing.js
+++ routes/processing.js
@@ -1,4 +1,11 @@
 exports.forecastProcessing = function(jsonString){
 	var objectJson = JSON.parse(jsonString);
 	console.log('objectJson : ' + objectJson);
+	// Creation of the response string :
+	var response = 'Météo à  ' + objectJson.city.name + ' : ';
+
+	// take the 9 first dt to be sure to have the wether for the day after : 
+
+	return response;
+
 };
