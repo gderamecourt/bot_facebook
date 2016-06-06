d9834eea65a27bb7fb4eeb4f92c1949e97cba676
gabouch <gabriel.deramecourt@gmail.com>
2016-06-06 20:59:13 +0200

    Third try Display of the temperature

--- routes/processing.js
+++ routes/processing.js
@@ -2,7 +2,7 @@ exports.forecastProcessing = function(jsonString){
 	var objectJson = JSON.parse(jsonString);
 	console.log('name : ' + objectJson.city.name);
 	// Creation of the response string :
-	var response = 'Météo à ' + objectJson.city.name + ' : \\n';
+	var response = 'Météo à ' + objectJson.city.name + ' : \u240D';
 
 	for (i = 0; i < 9; i++){
 		// table with the previsions for a delta time
@@ -13,7 +13,7 @@ exports.forecastProcessing = function(jsonString){
 		var curentDt = Math.floor(Date.now() / 1000);
 		// hours between the timestamps : 
 		var delta = Math.ceil((dt - current)/3600);
-		response += 'dans ' + delta + ' heures, il fera ' + values.main.temp + ' degrés \\n';
+		response += 'dans ' + delta + ' heures, il fera ' + values.main.temp + ' degrés \u240D';
 
 	}
 	// take the 9 first dt to be sure to have the weather for the day after : 
