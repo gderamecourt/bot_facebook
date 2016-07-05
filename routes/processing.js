3d74c783461bc5ba8ff7efc49cb606e0b227e8d3
gabouch <gabriel.deramecourt@gmail.com>
2016-07-05 22:59:38 +0200

    Addition of weather

--- routes/processing.js
+++ routes/processing.js
@@ -2,9 +2,9 @@ exports.forecastProcessing = function(jsonString){
 	var objectJson = JSON.parse(jsonString);
 	console.log('name : ' + objectJson.city.name);
 	// Creation of the response string :
-	var response = 'Météo à ' + objectJson.city.name + ' : \n';
+	var response = 'Météo à ' + objectJson.city.name + ' dans : \n';
 
-	for (i = 0; i < 9; i++){
+	for (i = 0; i < 8; i++){
 		// table with the previsions for a delta time
 		var values = objectJson.list[i];
 		// timestamp of the prevision
@@ -13,13 +13,53 @@ exports.forecastProcessing = function(jsonString){
 		var curentDt = Math.floor(Date.now() / 1000);
 		// hours between the timestamps : 
 		var delta = Math.ceil((dt - curentDt)/3600);
-		response += 'dans ' + delta.toString() + 'h : ' + 
-			values.main.temp.toString() + ' degrés \n';
+
+		var meteo = weatherToMeteo(values.weather[0].main);
+
+		response += delta.toString() + 'h : ' + 
+			values.main.temp.toString() + ' °, '+ meteo + '\n';
 
 	}
-	// take the 9 first dt to be sure to have the weather for the day after : 
+	// take the 8 first dt to be sure to have the weather for the day after : 
 
 
 	return response;
 
 };
+
+function weatherToMeteo(weather){
+	var meteo;
+	switch(weather.toUpperCase()){
+		case 'CLEAR SKY': 
+			meteo = 'ciel bleu';
+		break;
+		case 'FEW CLOUDS': 
+			meteo = 'quelques nuages';
+		break;
+		case 'SCATTERED CLOUDS': 
+			meteo = 'ciel ombragé';
+		break;
+		case 'BROKEN CLOUDS': 
+			meto = 'nuageux';
+		break;
+		case 'SHOWER RAIN': 
+			meteo = 'pluie torrentielle'
+		break;
+		case 'RAIN': 
+			meteo = 'pluie';
+		break;
+		case 'THUNDERSTORM': 
+			meteo = 'orage';
+		break;
+		case 'SNOW': 
+			meteo = 'neige';
+		break;
+		case 'MIST': 
+			meteo = 'brouillard';
+		break;
+
+		default: 
+			meteo = weather;
+	} 
+	return meteo;
+}
